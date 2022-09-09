e99cf23ecabc2b9d29a17c3527dabfef68228598
Paris Isaac <parisisaac65@gmail.com>
2022-09-09 15:48:40 -0400

    Add operate function that takes an operator and 2 numbers

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const add = function(a,b){
 
 const subtract = function(a,b){
     return a - b; 
-}
+};
 
 const multiply = function(a,b){
     return a * b; 
-}
+};
 
 const divide = function(a,b){
     return a / b; 
-}
+};
 
+const operate = function(sign, a, b){
+    if(sign == '+') return add(a,b); 
+    if(sign == '-') return subtract(a,b); 
+    if(sign == '*') return multiply(a,b);
+    if(sign == '/') return divide(a,b);  
+};
+
+console.log(operate('+',6, 2));
+console.log(operate('-',6, 2));
+console.log(operate('*',6, 2));
+console.log(operate('/',6, 2)); 
